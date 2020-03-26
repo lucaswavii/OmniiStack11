f697df323cc158d50f1ed771b09c3691946d15e3
lucas.ricarte <lucas.ricarte@valenca.com.br>
2020-03-26 19:40:16 -0300

    Backend da Aplicação

---
+++ backend/src/routes.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const OngController = require('../src/controllers/OngController');
+const IncidentsController = require('../src/controllers/IncidentsController');
+const ProfileController = require('../src/controllers/ProfileController');
+const SessionController = require('../src/controllers/SessionController');
+
+const routes = express.Router();
+
+routes.post('sessions', SessionController.create);
+
+routes.get('/profile'  , ProfileController.list        );
+
+routes.post('/ongs' , OngController.create      );
+routes.get('/ongs'  , OngController.list        );
+
+routes.post('/incidents'        , IncidentsController.create    );
+routes.get('/incidents'         , IncidentsController.list      );
+routes.delete('/incidents/:id'  , IncidentsController.delete    );
+
+module.exports = routes;
